57167e0b50e37b7601f15d8e5751b2e55f4e829d
Aisupurin <141869645+Aisupurin@users.noreply.github.com>
2024-10-08 12:11:02 -0300

    v

--- pergntas.js
+++ pergntas.js
@@ -8,3 +8,42 @@ criaCartao(
     'Matemática' ,
     'Qual a fórmula de Bhaskara?' ,
 )
+
+criaCartao(
+    'História',
+    'De quem é a famosa frase “Penso, logo existo”?' ,
+    'Descartes'
+)
+
+criaCartao(
+    'Historia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'historia',
+    'Quais o menor e o maior país do mundo?',
+    ' Vaticano e Rússia'
+)
+
+criaCartao(
+    'historia',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao(
+    'historia',
+    ' Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote'
+)
+
+criaCartao(
+    'Matematica',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
+
+criaCartao(
+    'historia',
