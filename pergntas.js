3b69537e92de0bf606ee5c010462894ba8cf1ddd
Aisupurin <141869645+Aisupurin@users.noreply.github.com>
2024-10-08 11:54:54 -0300

    erhs

---
+++ pergntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    'Matemática' ,
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1=an)).q)/2'
+)
+
+criaCartao(
+    'Matemática' ,
+    'Qual a fórmula de Bhaskara?' ,
+)
